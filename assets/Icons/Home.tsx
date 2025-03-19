a771fc4c053d3b2a9c134ef93b9b351e77690d15
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2025-03-18 20:52:13 -0600

    Add assets and icons

---
+++ assets/Icons/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+import Svg, { SvgProps, Path } from "react-native-svg"
+const SvgComponent = (props: SvgProps) => (
+  <Svg
+    fill="currentColor"
+    className="h-5 w-5"
+    viewBox="0 0 20 20"
+    width={24}
+    height={24}
+    {...props}
+  >
+    <Path d="M10.707 2.293a1 1 0 0 0-1.414 0l-7 7a1 1 0 0 0 1.414 1.414L4 10.414V17a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-6.586l.293.293a1 1 0 0 0 1.414-1.414l-7-7z" />
+  </Svg>
+)
+export default SvgComponent;
